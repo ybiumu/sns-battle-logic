436371bf46fbde0595250fa60f16de90178dcc49
ybiumu@gmail.com <ybiumu@gmail.com@ab01dbf2-b64e-df44-ea26-54ec00198987>
2013-10-10 06:58:26 +0000

    * backup after clash

--- perl-calc/.htcron/scheduler.sh
+++ perl-calc/.htcron/scheduler.sh
@@ -3,18 +3,18 @@
 
 HOUR=$(date +"%H");
 
-if [ "x${HOUR}" != "x" -a "${HOUR}" == "00" ];
+if [ "x${HOUR}" != "x" -a "${HOUR}" == "00" -o  "${HOUR}" == "04"  -o  "${HOUR}" == "08" -o  "${HOUR}" == "12" -o  "${HOUR}" == "16" -o  "${HOUR}" == "20" ];
 then
     /home/users/2/ciao.jp-anothark/web/.htcron/twitter.pl
 fi;
-
-if [ "x${HOUR}" != "x" -a "${HOUR}" == "04" ];
-then
-    /home/users/2/ciao.jp-anothark/web/.htcron/daily_bat.pl
-fi;
-
-
-
-/home/users/2/ciao.jp-anothark/web/.htcron/daily_bat.pl
-
+#
+#if [ "x${HOUR}" != "x" -a "${HOUR}" == "04" ];
+#then
+#    /home/users/2/ciao.jp-anothark/web/.htcron/daily_bat.pl
+#fi;
+#
+#
+#
+#/home/users/2/ciao.jp-anothark/web/.htcron/daily_bat.pl
+#
 
