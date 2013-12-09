98074911393c0b63b6de1b0e50197a5189e281eb
ybiumu@gmail.com <ybiumu@gmail.com@ab01dbf2-b64e-df44-ea26-54ec00198987>
2013-12-09 14:45:58 +0000

    * SmartPhone avilable 2.

---
+++ perl-calc/.htlib/LoggingObjMethod.pm
@@ -0,0 +1,129 @@
+package LoggingObjMethod;
+$|=1;
+
+use ObjMethod;
+use LocalConfig;
+use base qw( ObjMethod );
+
+our $access_log = $LocalConfig::ACCESS_LOG;
+our $system_log = $LocalConfig::SYSTEM_LOG;
+our $both = 0;
+
+
+sub new
+{
+    my $class = shift;
+    my $self = $class->SUPER::new();
+    bless $self, $class;
+
+#    $self->init();
+    return $self;
+}
+
+sub init
+{
+    my $class = shift;
+    $class->SUPER::init();
+    $class->setAccessLog($access_log);
+    $class->setSystemLog($system_log);
+    $class->setBoth($both);
+
+}
+
+sub setBoth
+{
+    my $class = shift;
+    return $class->setAttribute( 'both', shift );
+}
+
+sub getBoth
+{
+    return $_[0]->getAttribute( 'both' );
+}
+
+
+sub setSystemLog
+{
+    my $class = shift;
+    return $class->setAttribute( 'system_log', getLogPath(shift) );
+}
+
+sub getSystemLog
+{
+    return $_[0]->getAttribute( 'system_log' );
+}
+
+sub setAccessLog
+{
+    my $class = shift;
+    return $class->setAttribute( 'access_log', getLogPath(shift) );
+}
+
+sub getAccessLog
+{
+    return $_[0]->getAttribute( 'access_log' );
+}
+
+sub getLogPath
+{
+    return sprintf("%s/%s", $LocalConfig::LOG_DIR, shift );
+}
+
+sub output_access_log
+{
+    my $class = shift;
+    open LOG, ">>", $class->getAccessLog()  || ( printError("Can't open log 1") && exit );
+    printf LOG "[%s] %s\n", scalar(localtime()), join(" ",@_);
+    close LOG;
+}
+
+sub output_log
+{
+    my $class = shift;
+    open LOG, ">>", $class->getSystemLog() || die "Can't open log 2[" .$class->getSystemLog()  . "] msg:[".join("",@_)."]" ;
+    my $log_str = sprintf( "[%s] %s\n", scalar(localtime()), join("",@_) );
+    printf LOG $log_str;
+    printf $log_str if $class->getBoth();
+    $LocalConfig::LOCAL_DEBUG && warn $log_str; 
+    close LOG;
+}
+
+
+sub debug
+{
+    my $class = shift;
+    $class->output_log(sprintf("[DEBUG] %s", join("",@_) ));
+}
+
+sub notice
+{
+    my $class = shift;
+    $class->output_log(sprintf("[NOTICE] %s", join("",@_) ));
+}
+
+sub error
+{
+    my $class = shift;
+    $class->output_log(sprintf("[ERROR] %s", join("",@_) ));
+}
+
+sub warning
+{
+    my $class = shift;
+    $class->output_log(sprintf("[WARNING] %s", join("",@_) ));
+}
+
+
+sub printError
+{
+    my $class = shift;
+    my $error_str = join("",@_);
+    $class->error($error_str);
+print STDERR <<_HEADER_;
+$error_str
+_HEADER_
+    exit;
+}
+
+
+1;
---
+++ perl-calc/sp/callback.cgi
@@ -0,0 +1,68 @@
+#!/usr/bin/perl
+
+use lib qw( .htlib ../.htlib );
+use YAML::Tiny;
+use Net::Twitter::Lite;
+use CGI;
+use DbUtil;
+use LocalConfig;
+
+
+my $db = DbUtil::getDbHandler();
+use CGI::Session;
+
+my $cgi = new CGI;
+my $session = new CGI::Session(undef, undef, {Directory=>'/home/users/2/ciao.jp-anothark/web/.htsession'});
+$session->expire('+12h');
+
+############
+### Main ###
+############
+use strict;
+use warnings;
+use Encode;
+
+my $oauth_verifier = $cgi->param('oauth_verifier');
+
+#my $yaml = (YAML::Tiny->read('/home/users/2/ciao.jp-anothark/web/.htlib/twconf.yml'));
+my $config = ( YAML::Tiny->read('/home/users/2/ciao.jp-anothark/web/.htcron/twconf.yml') )->[0];
+#my $config = ( YAML::Tiny->read('twconf.yml') )->[0];
+#my $config = $yaml->[0];
+my $twit = Net::Twitter::Lite->new(
+    consumer_key    => $config->{'cs_key'},
+    consumer_secret => $config->{'cs_secret'},
+    legacy_lists_api => 1,
+    # For twitter api v1.1
+    apiurl                => 'http://api.twitter.com/1.1',
+    search_trends_api_url => 'http://api.twitter.com/1.1',
+    lists_api_url         => 'http://api.twitter.com/1.1'
+);
+
+
+
+$twit->request_token($cgi->cookie('token'));
+$twit->request_token_secret($cgi->cookie('token_secret'));
+my($access_token, $access_token_secret, $user_id, $screen_name) = $twit->request_access_token(verifier => $oauth_verifier);
+
+
+$session->param(-name=>'access_token', -value=>$access_token);
+$session->param(-name=>'access_token_secret', -value=>$access_token_secret);
+$session->param(-name=>'user_id', -value=>$user_id);
+$session->param(-name=>'screen_name', -value=>$screen_name);
+$session->param(-name=>'pattern', -value=>"twauth");
+
+my $cookie = CGI::Cookie->new(-name => 'CGISESSID',
+                             -value => $session->id(),
+                             -expires => '+1h',
+                             -path => '/sp'
+                             );
+
+print $cgi->header(
+  -cookie=>$cookie,
+  -location => $LocalConfig::BASE_URL . "/sp/mypage.cgi"
+);
+
+
+$db->disconnect();
+exit;
+
